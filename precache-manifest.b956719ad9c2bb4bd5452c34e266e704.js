eafb1854e8d828e8ed71678c11f2fab9598412ae
Gurpreet Singh <gurpreetrai000007@gmail.com>
2021-06-23 23:34:17 +0530

    Updates

---
+++ precache-manifest.b956719ad9c2bb4bd5452c34e266e704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a3c883f5fa83b52de579890f5a7a63",
+    "url": "/stockreact/index.html"
+  },
+  {
+    "revision": "c037921464daadbe3e18",
+    "url": "/stockreact/static/css/2.7c7f0f8c.chunk.css"
+  },
+  {
+    "revision": "e7623a2f5f52fa4e3767",
+    "url": "/stockreact/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "c037921464daadbe3e18",
+    "url": "/stockreact/static/js/2.5c9350f8.chunk.js"
+  },
+  {
+    "revision": "e7623a2f5f52fa4e3767",
+    "url": "/stockreact/static/js/main.04517cd1.chunk.js"
+  },
+  {
+    "revision": "a1a092d4605aaa024789",
+    "url": "/stockreact/static/js/runtime~main.0a4c27c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stockreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
